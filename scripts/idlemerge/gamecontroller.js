06c60abffb8d059ee15e6b876031b1b6b982f823
jinmoKim <roekdrnfl@naver.com>
2026-03-31 21:15:53 +0900

    Implement initial income login

--- scripts/idlemerge/gamecontroller.js
+++ scripts/idlemerge/gamecontroller.js
@@ -3,7 +3,7 @@ window.onload = function() {
         gen = document.querySelector("#generator"),
         remaining = document.querySelector("#remaining");
     
-    const max = 10, sizeOfBanana = 40;
+    const max = 10, sizeOfBanana = 40, sizeOfBranch = 25;
 
     let remain = 10;
 
@@ -22,7 +22,7 @@ window.onload = function() {
                 randomX + sizeOfBanana > window.innerWidth ?
                 window.innerWidth - sizeOfBanana : randomX
             ),
-            adjustedY = randomY - sizeOfBanana < 0 ? 0 :
+            adjustedY = randomY - sizeOfBanana - sizeOfBranch < 0 ? sizeOfBranch :
                 (
                     randomY + sizeOfBanana > window.innerHeight ?
                     window.innerHeight - sizeOfBanana : randomY
@@ -37,5 +37,36 @@ window.onload = function() {
         banana.style.left = `${adjustedX}px`;
 
         jungle.appendChild(banana);
+
+        setTimeout(() => {
+            const monkey = banana, sizeOfMonkey = sizeOfBanana * 1.5, income = 3;
+            monkey.src = "./images/monkey_lv1.png";
+            monkey.style.height = `${sizeOfMonkey}px`;
+            
+            const div = document.createElement("div");
+            div.style.position = "absolute";
+            div.style.zIndex = 0;
+            div.style.top = `${adjustedY - sizeOfBranch}px`;
+            div.style.left = `${adjustedX}px`;
+            div.style.display = "flex";
+            div.style.alignItems = "center";
+            div.style.color = "white";
+            div.style.fontWeight = "bold";
+
+            const branch = document.createElement("img");
+            branch.src = "./images/branch.png";
+            branch.style.height = `${sizeOfBranch}px`;
+            branch.style.zIndex = 0;
+
+            setTimeout(() => {
+                div.append(branch, ` +${income}`);
+                jungle.appendChild(div);
+            }, 800);
+
+            
+            setTimeout(() => {
+                div.remove();
+            }, 1600);
+        }, 1500);
     });
 }
